d57c62e4745497b65bbbdb4fea36a662b736e06c
Diego Augusto dos Santos <diegoaugustods1993@gmail.com>
2019-07-11 18:40:41 -0300

    Criar a class Animal

--- rePet/models.py
+++ rePet/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+class Animal(models.Model):
+
+    animal_tamanho = [
+        ('pp', 'Porte Pequeno'),
+        ('pm', 'Porte Médio'),
+        ('pg', 'Porte Grande'),
+    ]
+
+    animal_idade = [
+        ('0', 'De 2 a 6 meses'),
+        ('1', '1 Ano'),
+        ('2', '2 anos'),
+        ('3', '3 anos'),
+        ('4', '4 anos'),
+        ('5', '5 anos'),
+        ('6', '6 anos'),
+        ('7', '7 anos'),
+        ('8', '8 anos'),
+    ]
+    nome = models.CharField(max_length=30)
+    tamanho = models.CharField(max_length=20, choices=animal_tamanho)
+    idade = models.CharField(max_length= 20, choices=animal_idade)
+    foto = models.ImageField(upload_to='')
+
+    def __str__(self):
+        return self.nome
